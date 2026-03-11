65cbe411bbbb94b62ceb83e4ebc79dc2a0e3f961
Ayush Mishra <cyberfungus.07@gmail.com>
2026-03-11 07:38:29 +0530

    difference between the files motion and std was calculated

--- video_fix/jitter/analyse.py
+++ video_fix/jitter/analyse.py
@@ -53,7 +53,6 @@ video_dir = Path("/Users/ayushmishra06/Desktop/sign-language-video-fix/data/ai_g
 videos = list(video_dir.glob("*.mp4"))
 print(f"Found {len(videos)} videos\n")
 compute_transforms(videos)
-
 print("Ananlysing in the real video")
 video_dir = Path("/Users/ayushmishra06/Desktop/sign-language-video-fix/data/real")
 videos = list(video_dir.glob("*.mp4"))
